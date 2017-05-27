b1d277a995a7752674409d27d666b8d2f80cc3f8
charly23 <charlycapillanes@gmail.com>
2017-05-27 11:37:32 -0700

    add files

---
+++ scripts/scripts.js
@@ -0,0 +1,51 @@
+$(function()
+    {
+        
+        var $error_message = {'empty':'', 'wrong-password':'' };
+
+        $( '.login__form .login__form-pad' ).on ( 'click', '.login__form-submit input', function()
+            {
+
+                var $name = $( '.login__form-name input' );
+                if( $name.val().length !== 0 ) {
+                    $name.removeClass( 'form-name-validate' );
+                    $nm_login = 1;
+                } else {
+                    $name.addClass( 'form-name-validate' );
+                    $nm_login = 0;
+                }
+
+                var $password = $( '.login__form-password input' );
+                if( $password.val().length !== 0 ) {
+                    $password.removeClass( 'form-password-validate' );
+                    $pw_login = 1;
+                } else {
+                    $password.addClass( 'form-password-validate' );
+                    $pw_login = 0;
+                }
+
+                if( $nm_login == 1 ) {
+                    $( '.login__form' ).addClass( 'login-name-in' );
+                    $( '.home__pad' ).addClass( 'login-name-in' );
+                } else {
+                    $( '.login__form' ).removeClass( 'login-name-in' );
+                    $( '.home__pad' ).removeClass( 'login-name-in' );
+                }
+
+                if( $pw_login == 1 ) {
+                    $( '.login__form' ).addClass( 'login-password-in' );
+                    $( '.home__pad' ).addClass( 'login-password-in' );
+                } else {
+                    $( '.login__form' ).removeClass( 'login-password-in' );
+                    $( '.home__pad' ).removeClass( 'login-password-in' );
+                }
+
+                return false;
+            } 
+        );
+
+        console.log( 'scripts-loader' );
+
+        // END
+    }
+);
